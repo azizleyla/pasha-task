a68c14cddafe2986e1cbc2921d51324f774037ac
Leyla Aziz <ezizovaleyla1@gmail.com>
2023-03-03 00:30:07 +0400

    search func and pagination

--- src/component/table/Table.js
+++ src/component/table/Table.js
@@ -1,12 +1,13 @@
-import { Button, Modal, Table } from 'antd';
-import { useState } from 'react';
+import { Button, Input, Modal, Pagination, Table } from 'antd';
+import { useEffect, useState } from 'react';
 import DeleteModal from '../modal/DeleteModal';
-import Sidebar from '../sidebar/Sidebar';
 import { PlusOutlined } from "@ant-design/icons"
-import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { ApiQueryKeys } from '../../constants/api.constants';
 import { CustomerApi } from '../../api/customers.api';
 import StatusModal from '../modal/StatusModal';
+
+const { Search } = Input
 
 const onChange = (pagination, filters, sorter, extra) => {
   console.log('params', pagination, filters, sorter, extra);
@@ -21,33 +22,35 @@ const statusMap = {
 const TableList = () => {
   const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const[isOpenStatusModal,setIsOpenStatusModal] = useState(false);
-  const[selectedItem,setSelectedItem] = useState("")
-   const{data} = useQuery({
-    queryKey:[ApiQueryKeys.customers],
-    queryFn:() => CustomerApi.getAll(),
-    keepPreviousData:true
-   })
+  const [isOpenStatusModal, setIsOpenStatusModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("")
 
+  const [searchQuery, setSearchQuery] = useState("")
+  const { data } = useQuery({
+    queryKey: [ApiQueryKeys.customers],
+    queryFn: () => CustomerApi.getAll(),
+    keepPreviousData: true
+  })
 
-  const customers = data || [];
-const queryClient = useQueryClient()
-  const handleDeleteMutation = useMutation(CustomerApi.deleteCustomer,{
-    onSuccess:() =>{
+  const [customers, setCustomers] = useState(data || [])
+
+  const queryClient = useQueryClient()
+  const handleDeleteMutation = useMutation(CustomerApi.deleteCustomer, {
+    onSuccess: () => {
       queryClient.invalidateQueries([ApiQueryKeys.customers])
       setIsModalOpen(false)
     }
   })
-  const handleDelete = (value) =>{
+  const handleDelete = (value) => {
 
     setIsModalOpen(true)
     setSelectedItem(value)
-  
+
   }
 
-  const handleDeleteItem = () =>{
+  const handleDeleteItem = () => {
     handleDeleteMutation.mutate(selectedItem.id)
-    
+
   }
   const columns = [
     {
@@ -70,25 +73,34 @@ const queryClient = useQueryClient()
       title: 'Toplam məbləğ',
       dataIndex: 'totalPrice',
 
-      sorter: (a, b) => a.totalPrice- b.totalPrice
+      sorter: (a, b) => a.totalPrice - b.totalPrice
     },
     {
       title: "Status",
       dataIndex: "status",
       render: (value) =>
-        <p  className={`status-btn ${statusMap[value]}`}>{value}</p>
+        <p className={`status-btn ${statusMap[value]}`}>{value}</p>
 
     },
     {
       title: "action",
       render: (value) =>
-        <DeleteModal onDelete={() =>handleDelete(value)}  value={value} setSelectedItem={setSelectedItem} setIsOpenStatusModal={setIsOpenStatusModal} isOpenDeleteModal={isOpenDeleteModal} setIsOpenDeleteModal={setIsOpenDeleteModal} />
+        <DeleteModal onDelete={() => handleDelete(value)} value={value} setSelectedItem={setSelectedItem} setIsOpenStatusModal={setIsOpenStatusModal} isOpenDeleteModal={isOpenDeleteModal} setIsOpenDeleteModal={setIsOpenDeleteModal} />
 
     }
   ];
 
 
 
+  useEffect(() => {
+    if (searchQuery !== "") {
+
+      setCustomers(data.filter(x => x.fullName.toLowerCase().includes(searchQuery.toLowerCase())))
+    } else {
+      setCustomers(data || [])
+    }
+
+  }, [data, searchQuery])
 
   const handleOk = () => {
     setIsModalOpen(false);
@@ -100,19 +112,29 @@ const queryClient = useQueryClient()
   };
 
 
-console.log(data)
+
+  const onChange = (pageNumber) => {
+    console.log('Page: ', pageNumber);
+  };
   return (
     <div className='table-container'>
       <Button type="primary" className='add-btn'>
         <PlusOutlined />  Yeni Qaime </Button>
-      <Table columns={columns} dataSource={customers} onChange={onChange} />
+
+
+      <Search onChange={(e) => setSearchQuery(e.target.value)}
+        allowClear
+        value={searchQuery}
+        placeholder='Qaimə nömrəsi, müştəri adı üzrə axtar' />
+
+      <Table pagination={{ defaultPageSize: 10, showQuickJumper: true, total: customers.length, locale: { jump_to: "Səhifəyə get" } }} locale={{ jump_to: "sehife" }} columns={columns} dataSource={customers} onChange={onChange} />
       <Modal open={isModalOpen} onCancel={handleCancel} footer={[
         <Button key="1" onClick={() => setIsModalOpen(false)}>İmtina</Button>,
         <Button key="2" type='primary' onClick={handleDeleteItem}>Sil</Button>,
       ]} onOk={handleOk}>
         <p>Bu qaiməni silmək istədiyinizdən əminsiniz?</p>
       </Modal>
-      <StatusModal  setSelectedItem={setSelectedItem} selectedItem={selectedItem} isOpenStatusModal={isOpenStatusModal} setIsOpenStatusModal={setIsOpenStatusModal}/>
+      <StatusModal setSelectedItem={setSelectedItem} selectedItem={selectedItem} isOpenStatusModal={isOpenStatusModal} setIsOpenStatusModal={setIsOpenStatusModal} />
 
 
     </div>
